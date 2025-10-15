77fb65dc613164ed65a593844039c39c1a099533
Le Duong <leduongnewmemes123@gmail.com>
2025-10-15 16:13:54 +0700

    feat: Implement passport jwt configs

---
+++ backend/configs/passportConfig.js
@@ -0,0 +1,26 @@
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const db = require('../prisma/queries');
+require('dotenv').config();
+
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.SECRET,
+}
+
+const verifyCallback = async (jwt_payload, done) => {
+    try {
+        const user = await db.getUser(jwt_payload.id)
+        if (!user) {
+            return done(null, false)
+        } else {
+            return done(null, user)
+        }
+    } catch(err) {
+        console.log("Error at verifyCallback: ")
+        return done(err, false)
+    }
+}
+
+passport.use(new JwtStrategy(opts, verifyCallback))
