634b977b5f36fcc97a8edba5e63916ec1fb35eaf
Le Duong <leduongnewmemes123@gmail.com>
2025-10-15 15:23:47 +0700

    feat: Add configs

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Passport configs
+require('./configs/passportConfig')
 
 // Routes
 const indexRouter = require('./routes/indexRouter')
