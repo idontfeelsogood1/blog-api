b3e074a8840ab391d711b18954e90773ccd68db4
Le Duong <leduongnewmemes123@gmail.com>
2025-10-16 08:43:20 +0700

    feat: Implement routes for authenticated writer

---
+++ backend/routes/blogProtectedRouter.js
@@ -0,0 +1,25 @@
+const { Router } = require('express')
+const router = new Router()
+const blogController = require('../controllers/blogController')
+
+// Get all blogs
+router.get('/', blogController.allBlogGet)
+
+// Publish blog
+// router.put('/:blogId/publish', blogController.publishBlogPut)
+
+// Unpulish blog
+// router.put('/:blogId/unpublish', blogController.unpublishBlogPut)
+
+// Add new blog
+// router.post('/add', blogController.addBlogPost)
+
+// Delete comment
+// router.delete('/:blogId/:commentId/delete', blogController.commentDelete)
+
+// Not found
+router.use((req, res) => {
+    return res.sendStatus(404)
+})
+
+module.exports = router
