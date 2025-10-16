7814a7869724ceb5ff32be868612e3afeed7199a
Le Duong <leduongnewmemes123@gmail.com>
2025-10-16 12:31:11 +0700

    feat: Implement publishedBlogWithEverythingGet

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -12,15 +12,28 @@ async function allBlogGet(req, res) {
 
 async function allPublishedBlogGet(req, res) {
     try {
-        const publishedBlogs = await db.getAllPublishedBlog()
-        res.status(200).json({ blogs: publishedBlogs })
+        const blogs = await db.getAllPublishedBlog()
+        res.status(200).json({ blogs: blogs })
     } catch(err) {
         console.log(err)
         res.sendStatus(500)
     }
 }
 
+async function publishedBlogWithEverythingGet(req, res) {
+    try {
+        const blogId = parseInt(req.params.blogId)
+        const blogs = await db.getPublishedBlogWithEverything(blogId)
+        res.status(200).json({ blogs: blogs })
+    } catch(err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+}
+
+
 module.exports = {
     allBlogGet,
     allPublishedBlogGet,
+    publishedBlogWithEverythingGet,
 }
