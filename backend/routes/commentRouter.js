9fd3f04ceae0d6c421770a03a4088c31cf3dac7e
Le Duong <leduongnewmemes123@gmail.com>
2025-10-17 14:28:56 +0700

    feat: Finish route configs

--- backend/routes/commentRouter.js
+++ backend/routes/commentRouter.js
@@ -15,17 +15,17 @@ router.post('/add', passport.authenticate('jwt', { session: false }),
 )
 
 // PROTECTED ROUTE 
-// router.delete('/delete/:commentId', passport.authenticate('jwt', { session: false }), 
-//     (req, res, next) => {
-//         if (!req.user) {
-//             return res.sendStatus(401)
-//         } else if (!req.user.isWriter) {
-//             return res.sendStatus(403)
-//         }
-//         next()
-//     },
-//     commentController.commentDelete
-// )
+router.delete('/delete/:commentId', passport.authenticate('jwt', { session: false }), 
+    (req, res, next) => {
+        if (!req.user) {
+            return res.sendStatus(401)
+        } else if (!req.user.isWriter) {
+            return res.sendStatus(403)
+        }
+        next()
+    },
+    commentController.commentDelete
+)
 
 // Not found
 router.use((req, res) => {
