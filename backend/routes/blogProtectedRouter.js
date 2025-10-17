6ab8bc82c1f68ce2a135e6efc7e58c06f4d15074
Le Duong <leduongnewmemes123@gmail.com>
2025-10-17 13:01:52 +0700

    feat: Finish route implementation

--- backend/routes/blogProtectedRouter.js
+++ backend/routes/blogProtectedRouter.js
@@ -4,19 +4,16 @@ const blogController = require('../controllers/blogController')
 
 // Get all blogs
 router.get('/', blogController.allBlogGet)
-// router.get('/:blogId', blogController.blogWithEverythingGet)
+router.get('/:blogId', blogController.blogWithEverythingGet)
 
 // Publish blog
-// router.put('/:blogId/publish', blogController.publishBlogPut)
+router.put('/publish/:blogId', blogController.publishBlogPut)
 
 // Unpulish blog
-// router.put('/:blogId/unpublish', blogController.unpublishBlogPut)
+router.put('/unpublish/:blogId', blogController.unpublishBlogPut)
 
 // Add new blog
-// router.post('/add', blogController.addBlogPost)
-
-// Delete comment
-// router.delete('/:blogId/:commentId/delete', blogController.commentDelete)
+router.post('/add', blogController.addBlogPost)
 
 // Not found
 router.use((req, res) => {
