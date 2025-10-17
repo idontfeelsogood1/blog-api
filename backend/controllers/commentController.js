aeb3f89c5b04eff9b6558158b9ac52fd74f4b93e
Le Duong <leduongnewmemes123@gmail.com>
2025-10-17 13:50:39 +0700

    feat: Implement addCommentPost

---
+++ backend/controllers/commentController.js
@@ -0,0 +1,18 @@
+const db = require('../prisma/queries.js')
+
+async function addCommentPost(req, res) {
+    try {
+        const userId = req.user.id
+        const blogId = parseInt(req.params.blogId)
+        const body = req.body.body
+        await db.addComment(userId, blogId, body)
+        res.sendStatus(200)
+    } catch(err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+}
+
+module.exports = {
+    addCommentPost
+}
