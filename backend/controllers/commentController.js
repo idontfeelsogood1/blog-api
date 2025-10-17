8c2e8132794830c2062d65358cfc401a3c88828a
Le Duong <leduongnewmemes123@gmail.com>
2025-10-17 14:29:25 +0700

    feat: Implement commentDelete

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -13,6 +13,18 @@ async function addCommentPost(req, res) {
     }
 }
 
+async function commentDelete(req, res) {
+    try {
+        const commentId = parseInt(req.params.commentId)
+        await db.deleteComment(commentId)
+        res.sendStatus(200)
+    } catch(err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+}
+
 module.exports = {
     addCommentPost,
+    commentDelete,
 }
