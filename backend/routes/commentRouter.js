263947919ae19d6aca0db001fde6721f185a37d6
Le Duong <leduongnewmemes123@gmail.com>
2025-10-17 14:09:47 +0700

    feat: Implement public route

--- backend/routes/commentRouter.js
+++ backend/routes/commentRouter.js
@@ -1,10 +1,10 @@
 const { Router } = require('express')
-const router = new Router()
+const router = new Router({ mergeParams: true })
 const commentController = require('../controllers/commentController')
 const passport = require('passport')
 
 // PUBLIC ROUTE 
-router.post('/add', passport.authenticate(), 
+router.post('/add', passport.authenticate('jwt', { session: false }), 
     (req, res, next) => {
         if (!req.user) {
             return res.sendStatus(401)
@@ -15,14 +15,21 @@ router.post('/add', passport.authenticate(),
 )
 
 // PROTECTED ROUTE 
-router.delete('/delete/:commentId', passport.authenticate(), 
-    (req, res, next) => {
-        if (!req.user) {
-            return res.sendStatus(401)
-        } else if (!req.user.isWriter) {
-            return res.sendStatus(403)
-        }
-        next()
-    },
-    commentController.commentDelete
-)
+// router.delete('/delete/:commentId', passport.authenticate('jwt', { session: false }), 
+//     (req, res, next) => {
+//         if (!req.user) {
+//             return res.sendStatus(401)
+//         } else if (!req.user.isWriter) {
+//             return res.sendStatus(403)
+//         }
+//         next()
+//     },
+//     commentController.commentDelete
+// )
+
+// Not found
+router.use((req, res) => {
+    return res.sendStatus(404)
+})
+
+module.exports = router
