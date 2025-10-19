ceff0834eb219ba5fa1368ca27a589b2c0adaa9a
Le Duong <leduongnewmemes123@gmail.com>
2025-10-19 13:51:11 +0700

    feat(userBlog): Basic HTML

---
+++ userBlog/src/components/Index/Index.jsx
@@ -0,0 +1,14 @@
+export default function Index() {
+    return (
+        <div>
+            <h1>Welcome to the User Blog!</h1>
+            <p>This is a project created for The Odin Project's 
+                Node.js course to build an API server and two 
+                front-end websites (this one and an Admin Dashboard) 
+                to make requests to the API endpoints to view and 
+                manage blog posts, comments, and users.</p>
+            <p>The server is built with Node.js and Express, and both front-end sites in React.</p>
+            <p>To learn more about the whole project and how it is built, visit the About page.</p>
+        </div>
+    )
+}
