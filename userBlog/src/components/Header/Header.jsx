c3202d9a26d976caf22a7a7148baac3dbca06238
Le Duong <leduongnewmemes123@gmail.com>
2025-10-20 17:47:36 +0700

    feat(userBlog): Add class to elements for styling

--- userBlog/src/components/Header/Header.jsx
+++ userBlog/src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router"
+import style from "./Header.module.css"
 
 export default function Header({ user }) {
     return (
         <nav>
-            <Link to="/">User Blog - Blog API</Link>
-            <div>
-                <Link to="/about">About</Link>
-                <Link to="/blog">Blog</Link>
+            <Link className={style.indexLink} to="/">User Blog - Blog API</Link>
+            <div className={style.linkContainer}>
+                <Link className={style.link} to="/about">About</Link>
+                <Link className={style.link} to="/blog">Blog</Link>
                 { user
-                    ? <Link to="/logout">logout</Link>
-                    : <Link to="/login">login</Link>
+                    ? <Link className={style.link} to="/logout">logout</Link>
+                    : <Link className={style.link} to="/login">login</Link>
                 }
             </div>
         </nav>
