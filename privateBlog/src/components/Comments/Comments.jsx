d5fc8765baf7df0f0390d7cb9c9f651aba611570
Le Duong <leduongnewmemes123@gmail.com>
2025-10-22 14:41:14 +0700

    feat(privateBlog): Implement component

--- privateBlog/src/components/Comments/Comments.jsx
+++ privateBlog/src/components/Comments/Comments.jsx
@@ -1,5 +1,65 @@
+import style from "./Comments.module.css"
+import { fetchComments } from "../../api/fetch"
+import { useEffect, useState } from "react"
+
 export default function Comments() {
-    return (
-        <h1>comments</h1>
-    )
+    const [comments, setComments] = useState([])
+    const [isLoading, setLoading] = useState(true)
+
+    function deleteComment(commentId) {
+        console.log(commentId)
+    }
+
+    useEffect(() => {
+        const callback = async () => {
+            try {
+                const comments = await fetchComments()
+                setComments(comments)
+                setLoading(false)
+            } catch(err) {
+                console.log("Error at Comments: ", err)
+            }
+        }
+        callback()
+    }, [])
+
+    if (isLoading) {
+        return (
+            <div>
+                <h1>Blogs</h1>
+                <h2 className={style.loading}>Loading Blogs...</h2>
+            </div>
+        )
+    } else {
+        return (
+            <div className={style.container}>
+                <h1>Comments</h1>
+                <table className={style.commentContainer}>
+                    <thead>
+                        <tr>
+                            <th scope="col">Comment</th>
+                            <th scope="col">Author</th>
+                            <th scope="col">Blog</th>
+                            <th scope="col">Date</th>
+                            <th scope="col">Options</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {comments.map((comment) => {
+                            const formattedDate = comment.createAt.slice(0, 10)
+                            return (
+                                <tr>
+                                    <td className={style.body}>{comment.body}</td>
+                                    <td className={style.author}>{comment.user.username}</td>
+                                    <td className={style.blog}>{comment.blog.title}</td>
+                                    <td className={style.date}>{formattedDate}</td>
+                                    <td className={style.delete} onClick={() => {deleteComment(comment.id)}}><button>Delete</button></td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 }
