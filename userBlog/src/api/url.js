d648bcbaa85a3f51b8022a9b3ca98640903f309c
Le Duong <leduongnewmemes123@gmail.com>
2025-10-22 17:49:47 +0700

    feat(userBlog): Change backend url to render

--- userBlog/src/api/url.js
+++ userBlog/src/api/url.js
@@ -1,4 +1,4 @@
-export const verifyUrl = "http://localhost:3000/user/verify"
-export const blogsUrl = "http://localhost:3000/blog/"
-export const loginUrl = "http://localhost:3000/user/login"
-export const registerUrl = "http://localhost:3000/user/register"
+export const verifyUrl = "https://blog-api-1jyk.onrender.com/user/verify"
+export const blogsUrl = "https://blog-api-1jyk.onrender.com/blog/"
+export const loginUrl = "https://blog-api-1jyk.onrender.com/user/login"
+export const registerUrl = "https://blog-api-1jyk.onrender.com/user/register"
