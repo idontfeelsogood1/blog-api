d751579bd9881ebd5cbbfec1f13d35d2c25de3eb
Le Duong <leduongnewmemes123@gmail.com>
2025-10-22 14:04:19 +0700

    feat(privateBlog): Implement component

--- privateBlog/src/components/AddBlog/AddBlog.jsx
+++ privateBlog/src/components/AddBlog/AddBlog.jsx
@@ -1,5 +1,42 @@
+import { useNavigate } from "react-router"
+import { addBlog } from "../../api/update"
+
 export default function AddBlog() {
+    const navigate = useNavigate()
+
+    function handleSubmit(event) {
+        const callback = async () => {
+            try {
+                event.preventDefault()
+                const title = event.target.title.value
+                const published = event.target.published.value
+                const body = event.target.body.value
+                console.log(published)
+                await addBlog(title, published, body)
+                navigate("/blogs")
+            } catch(err) {
+                console.log("Error at AddPost: ", err)
+            }  
+        }
+        callback()
+    }
+
     return (
-        <h1>addblog</h1>
+        <div>
+            <a href="/blogs">View All Blogs</a>
+            <h1>Create New Blog</h1>
+            <form onSubmit={handleSubmit}>
+                <p><label htmlFor="title">Title:</label></p>
+                <input type="text" name="title" id="title" />
+                <p><label htmlFor="published">Set Status</label></p>
+                <select name="published" id="published">
+                    <option value="false">Unpublished</option>
+                    <option value="true">Published</option>
+                </select>
+                <p><label htmlFor="body">Blog Content:</label></p>
+                <textarea name="body" id="body"></textarea>
+                <button type="submit">Create Blog</button>
+            </form>
+        </div>
     )
 }
